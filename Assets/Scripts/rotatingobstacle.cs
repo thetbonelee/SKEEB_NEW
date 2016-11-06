f180232030fcd88f353aca352c70de5c2f514930
Brett Mortensen <brettmortensen@users.noreply.github.com>
2016-11-06 15:17:42 -0700

    Add files via upload
    Speed and directions (x,y,z) can all be changed through the menu

---
+++ Assets/Scripts/rotatingobstacle.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class rotatingobstacle : MonoBehaviour {
+	public float speed = 25f;
+	public Vector3 directions;
+
+	void Update () {
+		transform.Rotate(Time.deltaTime * speed * directions);
+	}
+}
