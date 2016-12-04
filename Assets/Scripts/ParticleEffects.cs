a66792a76294a9029c488f5f9da8d398ae732eb8
Garrett Glad <garrett.glad@ubnt.com>
2016-12-04 11:18:23 -0700

    Added multiplier power up -- scenes 3+

---
+++ Assets/Scripts/ParticleEffects.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ParticleEffects : MonoBehaviour {
+
+	public GameObject collider;
+	public bool selected = false;
+	private bool started = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (selected && !started) {
+			collider.GetComponent<score> ().selectedMult = true;
+			StartEffect ();
+		}
+	}
+
+	void StartEffect (){
+		this.GetComponent<ParticleSystem> ().Play ();
+		started = true;
+	}
+}
--- Assets/Scripts/score.cs
+++ Assets/Scripts/score.cs
@@ -7,12 +7,19 @@ public class score : MonoBehaviour
 
     public int scoreValue = 0;
     public string level;
+	public bool selectedMult = false;
     public TextMesh scoreboard;
     public TextMesh ballCount;
     public GameObject ball;
     public GameObject launcher;
     public GameObject[] launchers;
     public static int balls = 8;
+	public static int multiplier = 1;
+	public ParticleSystem ps;
+	private bool psStarted = false;
+	//private int multiplier = 1;
+	//public static int prevScore = 0;
+	public const int MAX_MULT = 8;
 
     void OnTriggerEnter(Collider other)
     {
@@ -22,7 +29,20 @@ public class score : MonoBehaviour
         {
             Destroy(launch);
         }
-        scoreboard.text = (int.Parse(scoreboard.text) + scoreValue).ToString().PadLeft(4, '0');
+
+		int score = scoreValue * multiplier;
+		Debug.Log (multiplier);
+
+		if (selectedMult) {
+			if (multiplier < MAX_MULT) {
+				multiplier *= 2;
+			}
+		} else {
+			multiplier = 1;
+		}
+		//Debug.Log (multiplier);
+		//scoreValue *= multiplier;
+        scoreboard.text = (int.Parse(scoreboard.text) + score).ToString().PadLeft(4, '0');
         if (scoreValue != 0)
         {
             ballCount.text = "BALLS: " + (--balls).ToString();
@@ -45,4 +65,22 @@ public class score : MonoBehaviour
         Instantiate(launcher, new Vector3(0.011f, 0.393f, -10.721f), transform.rotation);
         Invoke("Reload", 3);
     }
+
+	void Update()
+	{
+		/*if (!psStarted) {
+			StartPS ();
+		}*/
+	}
+
+	void StartPS()
+	{
+		ps.Emit (2000);
+		psStarted = true;
+	}
+
+	void SetSelected(bool select)
+	{
+		selectedMult = select;
+	}
 }
